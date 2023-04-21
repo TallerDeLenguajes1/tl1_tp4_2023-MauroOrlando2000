6adb6f117c60704deed81ca30c31b17f7f517ef3
Mauro Orlando <orlandomauro2000@gmail.com>
2023-04-21 11:28:47 -0300

    Punto 2, apartado 5 completo

--- tp4_2.c
+++ tp4_2.c
@@ -8,52 +8,110 @@ struct Tarea {
     int Duracion;                   // entre 10 – 100
 } typedef Tarea;
 
-void mostrarTareas(Tarea **punto, int num);
+void cargarTareas(Tarea **tareaPendiente, int num);
+void pendienteToRealizada(Tarea **tareaPendiente, Tarea **tareaRealizada, int num);
+void mostrarTareas(Tarea **tareaPendiente, Tarea **tareaRealizada, int num);
 
 int main()
 {
-    Tarea **punto;
-    int num, i;
-    char *Buff;
+    Tarea **trabajoPendiente, **trabajoRealizado;
+    int num;
 
     printf("Cuantas tareas debe realizar?: ");
     scanf("%d", &num);
-    fflush(stdin);
-    punto = (Tarea **)malloc(num*sizeof(Tarea *));
+    trabajoPendiente = (Tarea **)malloc(num*sizeof(Tarea *));
+    trabajoRealizado = (Tarea **)malloc(num*sizeof(Tarea *));
 
-    for(i=0; i<num; i++)
-    {
-        punto[i] = NULL;
-    }
+    cargarTareas(trabajoPendiente, num);
+    pendienteToRealizada(trabajoPendiente, trabajoRealizado, num);
+    mostrarTareas(trabajoPendiente, trabajoRealizado, num);
 
-    for(i=0; i<num; i++)
-    {
-        Buff = (char *)malloc(50*sizeof(char));
-        printf("\nTarea %d\n", i+1);
-        punto[i]->TareaID = i+1;
-        printf("\nDescripcion:\n");
-        gets(Buff);
-        punto[i]->Descripcion = (char *)malloc((strlen(Buff)+1)*sizeof(char));
-        strcpy(punto[i]->Descripcion, Buff);
-        printf("Duracion: ");
-        scanf("%d", punto[i]->Duracion);
-        fflush(stdin);
-        printf("\n");
-        free(Buff);
-    }
-    mostrarTareas(punto, num);
-    free(punto);
+    free(trabajoPendiente);
+    free(trabajoRealizado);
+
     return 0;
 }
 
-void mostrarTareas(Tarea **punto, int num)
+void cargarTareas(Tarea **tareaPendiente, int num)
 {
     int i;
+    char *Buff;
 
     for(i=0; i<num; i++)
     {
-        printf("\nTarea %d\n", punto[i]->TareaID);
-        printf("%s\n", punto[i]->Descripcion);
-        printf("Duracion: %d horas", punto[i]->Duracion);
+        tareaPendiente[i] = NULL;
+    }
+
+    for(i=0; i<num; i++)
+    {
+        tareaPendiente[i] = (Tarea *)malloc(sizeof(Tarea));
+        fflush(stdin);
+        printf("\nTarea %d", i+1);
+        tareaPendiente[i]->TareaID = i+1;
+        Buff = (char *)malloc(50*sizeof(char));
+        printf("\nDescripcion: ");
+        gets(Buff);
+        tareaPendiente[i]->Descripcion = (char *)malloc((strlen(Buff)+1)*sizeof(char));
+        strcpy(tareaPendiente[i]->Descripcion, Buff);
+        printf("Duracion: ");
+        scanf("%d", &tareaPendiente[i]->Duracion);
+        printf("\n");
+        free(Buff);
+    }
+}
+
+void pendienteToRealizada(Tarea **tareaPendiente, Tarea **tareaRealizada, int num)
+{
+    int i, aux;
+
+    for(i=0; i<num; i++)
+    {
+        tareaRealizada[i] = NULL;
+    }
+
+    for(i=0; i<num; i++)
+    {
+        aux = 0;
+        printf("\nTarea %d\n", tareaPendiente[i]->TareaID);
+        printf("%s\n", tareaPendiente[i]->Descripcion);
+        printf("Duracion: %d\n", tareaPendiente[i]->Duracion);
+        printf("Se realizo esta tarea? (1=Si, 0=No): ");
+        scanf("%d", &aux);
+        if(aux)
+        {
+            tareaRealizada[i] = (Tarea *)malloc(sizeof(Tarea));
+            tareaRealizada[i]->TareaID = tareaPendiente[i]->TareaID;
+            tareaRealizada[i]->Descripcion = (char *)malloc(strlen(tareaPendiente[i]->Descripcion)*sizeof(char));
+            strcpy(tareaRealizada[i]->Descripcion, tareaPendiente[i]->Descripcion);
+            tareaRealizada[i]->Duracion = tareaPendiente[i]->Duracion;
+            free(tareaPendiente[i]);
+            tareaPendiente[i] = NULL;
+        }
+    }
+}
+
+void mostrarTareas(Tarea **tareaPendiente, Tarea **tareaRealizada, int num)
+{
+    int i;
+
+    printf("\n\n****Tareas realizadas****");
+    for(i=0; i<num; i++)
+    {
+        if(tareaRealizada[i] != NULL)
+        {
+            printf("\nTarea %d\n", tareaRealizada[i]->TareaID);
+            printf("%s\n", tareaRealizada[i]->Descripcion);
+            printf("Duracion: %d\n\n", tareaRealizada[i]->Duracion);
+        }
+    }
+    printf("****Tareas pendientes****");
+    for(i=0; i<num; i++)
+    {
+        if(tareaPendiente[i] != NULL)
+        {
+            printf("\nTarea %d\n", tareaPendiente[i]->TareaID);
+            printf("%s\n", tareaPendiente[i]->Descripcion);
+            printf("Duracion: %d\n\n", tareaPendiente[i]->Duracion);
+        }
     }
 }
